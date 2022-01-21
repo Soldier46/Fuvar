e0604ec0a6ffbaa6e48d89632ab2f5857558491a
Soldier46 <rschuszler46@gmail.com>
2022-01-21 11:01:18 +0100

    2. feladat

--- Fuvar/Class1.cs
+++ Fuvar/Class1.cs
@@ -8,5 +8,30 @@ namespace Fuvar
 {
     class Class1
     {
+        public int TaxiId { get; set; }
+        public DateTime indIdo { get; set; }
+        public int utazasidotar { get; set; }
+
+        public double megtávolság { get; set; }
+
+        public double viteldíj { get; set; }
+
+        public double borravalo { get; set; }
+
+        public string fizetesmodja { get; set; }
+
+
+        public Class1(string valami)
+        {
+            string[] s = valami.Split(';');
+            TaxiId = int.Parse(s[0]);
+            indIdo = DateTime.Parse(s[1]);
+            utazasidotar = int.Parse(s[2]);
+            megtávolság = double.Parse(s[3]);
+            viteldíj = double.Parse(s[4]);
+            borravalo = double.Parse(s[5]);
+            fizetesmodja = s[6];
+        }
+
     }
 }
--- Fuvar/Program.cs
+++ Fuvar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Fuvar
 {
@@ -10,6 +11,15 @@ namespace Fuvar
     {
         static void Main(string[] args)
         {
+
+            List<Class1> EzANeveAlistanak = new List<Class1>();
+            foreach (var b in File.ReadAllLines("fuvar.csv").Skip(1)) 
+            {
+                EzANeveAlistanak.Add(new Class1(b));
+            }
+            
+
+            
             
         }
     }
